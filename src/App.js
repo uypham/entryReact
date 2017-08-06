750550efff9df2e14d87b62a8236b54c36d9a1b1
Uy Pham Vuong <uyphamvuong@gmail.com>
2017-08-06 12:35:57 +0700

    Upload React Project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div id="abc" className="testimg">
+          <a href="">dmfsdmfdfdf uyl</a>
+          <img src="/img/logo.svg" alt="" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+// var element = React.createElement("h1", { className: 'greeting' }, 'Hello, world!');
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
